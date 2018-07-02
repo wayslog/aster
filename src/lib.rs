7d68c81ded6949324fa734a0ccad522a8fecd1a3
wayslog <wayslog@wtp-430.wayslog.com>
2018-07-03 02:05:39 +0800

    add handler of proxy for client.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ extern crate env_logger;
 #[macro_use]
 extern crate log;
 extern crate bytes;
-extern crate crossbeam;
 extern crate num_cpus;
 #[macro_use]
 extern crate lazy_static;
@@ -16,6 +15,12 @@ pub mod com;
 
 pub use com::*;
 
+use std::mem;
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+use futures::future::join_all;
+use futures::unsync::mpsc::{channel, Receiver, Sender};
 use futures::{Async, Future, IntoFuture, Poll, Stream};
 
 use bytes::{BufMut, BytesMut};
@@ -29,7 +34,7 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 
 const delim: u8 = '\n' as u8;
-const space: u8 = ' ' as u8;
+const SPACE: u8 = ' ' as u8;
 
 pub fn proxy() -> Result<()> {
     env_logger::init();
@@ -37,45 +42,86 @@ pub fn proxy() -> Result<()> {
     Ok(())
 }
 
-fn handle(addr: &str) -> Result<()> {
-    let cluster = Arc::new(Cluster {
-        cc: ClusterConfig {},
-    });
+fn handle(addr: &str) -> std::result::Result<(), ()> {
+    let (tx, rx) = channel(1024);
+    let cluster = Rc::new(RefCell::new(Cluster {
+        cc: ClusterConfig {
+            servers: Vec::new(),
+        },
+    }));
+
+    let pcluster = cluster.clone();
+    let dcluster = cluster.clone();
+
+    let dispatch = Dispatch {
+        cluster: dcluster,
+        rx: rx,
+        txs: vec![tx.clone()],
+    };
+
+    // TODO: add cluster to executor
+    let nodes: Vec<_> = (&*pcluster.borrow())
+        .cc
+        .servers
+        .clone()
+        .into_iter()
+        .map(|server| forword(server.to_owned()))
+        .collect();
 
     let taddr = addr.parse().expect("bad listen address");
     let listener = TcpListener::bind(&taddr).unwrap();
     let server = listener
         .incoming()
         .map_err(|e| error!("accept fail: {}", e))
-        .for_each(|sock| {
-            let exec = Executor {
+        .for_each(move |sock| {
+            let handler = Handler {
+                tx: tx.clone(),
                 cluster: cluster.clone(),
+                state: State::Empty,
                 sock: Socket {
                     sock: sock,
                     buf: BytesMut::new(),
                     eof: false,
                 },
             };
-            tokio::executor::spawn(exec)
+            tokio::executor::current_thread::spawn(handler);
+            Ok(())
         });
-    // TODO: add cluster to executor
-    tokio::run(server);
-    Ok(())
+
+    let amt = join_all(nodes).join3(dispatch, server).map(|_| ());
+    tokio::executor::current_thread::block_on_all(amt)
 }
 
-pub struct Executor {
-    cluster: Arc<Cluster>,
-    sock: Socket,
+fn forword(
+    addr: String,
+) -> impl Future<Item = tokio::executor::Spawn, Error = ()> + 'static + Send {
+    let taddr = addr.parse().expect("bad server address");
+    TcpStream::connect(&taddr)
+        .map(move |sock| {
+            let node = Executor {
+                addr: addr.to_owned(),
+                sock: Socket {
+                    buf: BytesMut::new(),
+                    sock: sock,
+                    eof: false,
+                },
+            };
+            tokio::executor::spawn(node)
+        })
+        .map_err(|err| error!("erro {:?}", err))
 }
 
 pub struct Socket {
     buf: BytesMut,
     sock: TcpStream,
-
     eof: bool,
 }
 
 impl Socket {
+    fn buffer_mut(&mut self) -> &mut BytesMut {
+        &mut self.buf
+    }
+
     fn buffer(&self) -> &BytesMut {
         &self.buf
     }
@@ -107,28 +153,115 @@ impl Socket {
     }
 }
 
-impl Future for Executor {
+pub struct Handler {
+    cluster: Rc<RefCell<Cluster>>,
+    sock: Socket,
+    tx: Sender<MsgBatch>,
+    batch: MsgBatch,
+}
+
+impl Future for Handler {
     type Item = ();
     type Error = ();
 
     fn poll(&mut self) -> std::result::Result<Async<Self::Item>, Self::Error> {
-        Ok(Async::NotReady)
+        loop {
+            match self.batch.decode(self.sock.buffer_mut()) {
+                Ok(size) => {
+                    trace!("decoded msg: {}", size);
+                    self.sock.buf.advance(size);
+                },
+                Err(Error::MoreData)  => {
+                    let mut batch = MsgBatch::default();
+                    mem::swap(&mut self.batch, &mut batch);
+                    self.tx.send(batch);
+                }
+                Err(err) => {
+                    error!("fail to parse {:?}", err);
+                },
+            };
+            let _size = try_ready!(self.sock.poll_read().map_err(|err| {
+                error!("error when proxy read: {:?}", err);
+            }));
+        }
+
     }
 }
 
-pub struct ClusterConfig {}
+pub struct ClusterConfig {
+    servers: Vec<String>,
+}
 
 pub struct Cluster {
     cc: ClusterConfig,
 }
 
-pub struct Node {
+unsafe impl Send for Cluster {}
+
+pub struct Dispatch {
+    cluster: Rc<RefCell<Cluster>>,
+    rx: Receiver<MsgBatch>,
+    txs: Vec<Sender<MsgBatch>>,
+}
+
+impl Future for Dispatch {
+    type Item = ();
+    type Error = ();
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        Ok(Async::NotReady)
+    }
+}
+
+pub struct Executor {
     addr: String,
+    sock: Socket,
+}
+
+impl Future for Executor {
+    type Item = ();
+    type Error = ();
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        Ok(Async::NotReady)
+    }
 }
 
 pub struct MsgBatch {
     buf: BytesMut,
-    count: usize,
+    reqs: Vec<MCReq>,
+    cursor: usize,
+}
+
+impl MsgBatch {
+    pub fn len(&self) -> usize {
+        self.cursor
+    }
+
+    fn push(&mut self, req: MCReq) {
+        self.reqs.push(req)
+    }
+
+    fn decode(&mut self, data:&mut [u8]) -> Result<usize> {
+        loop {
+            if self.cursor == data.len() {
+                return Ok(self.len());
+            }
+            let req = MCReq::decode(&mut data[self.cursor..])?;
+            self.cursor+= req.len();
+            self.reqs.push(req);
+        }
+    }
+}
+
+impl Default for MsgBatch {
+    fn default() -> Self {
+        MsgBatch {
+            buf: BytesMut::new(),
+            reqs: Vec::new(),
+            cursor: 0,
+        }
+    }
 }
 
 pub trait Msg {
@@ -178,25 +311,23 @@ lazy_static!{
     static ref CMDS: Vec<&'static str> = {
         vec![
             // storage cmd
-            "set", // 0
-            "add",
-            "replace",
-            "append",
-            "prepend",
-            "cas", // 5
+            // 0 ~ 5
+            "set", "add", "replace", "append", "prepend", "cas",
             // retrieval cmd
-            "get", // 6
-            "gets", // 7
+            // 6 ~ 7
+            "get", "gets",
             // delete cmd,
-            "delete", // 8
+            // 8
+            "delete",
             // incr,decr cmd
-            "incr", // 9
-            "decr", // 10
+            // 9 ~ 10
+            "incr", "decr",
             // touch
-            "touch", // 11
+            // 11
+            "touch",
             // get and touch
-            "gat", // 12
-            "gats", // 13
+            // 12 ~ 13
+            "gat", "gats",
         ]
     };
     static ref CMD_MAP: AcAutomaton<&'static str> = {
@@ -204,26 +335,34 @@ lazy_static!{
     };
 }
 
-pub struct GetAndTouch {
-    pub cmd: Range,
+pub struct GatCmd {
+    pub cmd: Cmd,
     pub expire: Range,
+}
+
+pub struct Cmd {
+    pub cmd: Range,
     pub key: Range,
     pub data: Vec<u8>,
     pub done: AtomicBool,
 }
 
 pub enum MCReq {
-    Msg {
-        cmd: Range,
-        key: Range,
-        data: Vec<u8>,
-        done: AtomicBool,
-    },
-    Gat(Vec<GetAndTouch>),
+    Msg(Cmd),
+    Gat(Vec<GatCmd>),
     Sub(Vec<MCReq>),
 }
 
 impl MCReq {
+    pub fn len(&self) -> usize {
+        match &self {
+            MCReq::Msg(cmd) => cmd.data.len(),
+            MCReq::Gat(cmds) => cmds.get(0).map(|gat| gat.cmd.data.len()).unwrap(),
+            MCReq::Sub(cmds) => cmds.get(0).map(|sub| sub.len()).unwrap(),
+        }
+    }
+
+
     pub fn decode(src: &mut [u8]) -> Result<Self> {
         let pos = position(src, delim).ok_or(Error::BadMsg)?;
 
@@ -248,7 +387,7 @@ impl MCReq {
         let mut i = expire.1 + 1;
         let cum = {
             let key_bytes = &src[i..end];
-            count(key_bytes, space)
+            count(key_bytes, SPACE)
         };
         let mut subs = Vec::with_capacity(cum);
         let key_end = end - 2;
@@ -263,12 +402,14 @@ impl MCReq {
             let ptr = src.as_mut_ptr();
             let data = unsafe { Vec::from_raw_parts(ptr, end, end) };
 
-            let sub = GetAndTouch {
-                cmd: cmd.clone(),
-                key: key,
+            let sub = GatCmd {
+                cmd: Cmd {
+                    cmd: cmd.clone(),
+                    key: key,
+                    data: data,
+                    done: AtomicBool::new(false),
+                },
                 expire: expire.clone(),
-                data: data,
-                done: AtomicBool::new(false),
             };
             subs.push(sub);
         }
@@ -285,12 +426,12 @@ impl MCReq {
         let ptr = src.as_mut_ptr();
         let data = unsafe { Vec::from_raw_parts(ptr, len, len) };
 
-        Ok(MCReq::Msg {
+        Ok(MCReq::Msg(Cmd {
             cmd: cmd,
             key: key,
             data: data,
             done: AtomicBool::new(false),
-        })
+        }))
     }
 
     fn decode_one_line(src: &mut [u8], cmd: Range, end: usize) -> Result<MCReq> {
@@ -299,12 +440,12 @@ impl MCReq {
         let ptr = src.as_mut_ptr();
         let data = unsafe { Vec::from_raw_parts(ptr, end, end) };
 
-        Ok(MCReq::Msg {
+        Ok(MCReq::Msg(Cmd {
             cmd: cmd,
             key: key,
             data: data,
             done: AtomicBool::new(false),
-        })
+        }))
     }
 
     fn decode_retrieval(src: &mut [u8], cmd: Range, end: usize) -> Result<MCReq> {
@@ -312,7 +453,7 @@ impl MCReq {
 
         let cum = {
             let key_bytes = &src[i..end];
-            count(key_bytes, space)
+            count(key_bytes, SPACE)
         };
 
         let mut subs = Vec::with_capacity(cum);
@@ -325,28 +466,28 @@ impl MCReq {
 
             let key = MCReq::get_field(&src[i..key_end], i)?;
             i = key.1 + 1;
-            // let j = position(&src[i..key_end], space).ok_or(Error::BadKey)?;
+            // let j = position(&src[i..key_end], SPACE).ok_or(Error::BadKey)?;
             // let key = Range(i, j);
 
             let ptr = src.as_mut_ptr();
             let data = unsafe { Vec::from_raw_parts(ptr, end, end) };
 
-            let sub = MCReq::Msg {
+            let sub = MCReq::Msg(Cmd {
                 cmd: cmd.clone(),
                 key: key,
                 data: data,
                 done: AtomicBool::new(false),
-            };
+            });
             subs.push(sub);
         }
         Ok(MCReq::Sub(subs))
     }
 
     fn decode_len(src: &[u8], cas: bool) -> Result<usize> {
-        let k = rposition(&src[..], space).ok_or(Error::BadMsg)?;
-        let j = rposition(&src[..k], space).ok_or(Error::BadMsg)?;
+        let k = rposition(&src[..], SPACE).ok_or(Error::BadMsg)?;
+        let j = rposition(&src[..k], SPACE).ok_or(Error::BadMsg)?;
         if cas {
-            let i = rposition(&src[..j], space).ok_or(Error::BadMsg)?;
+            let i = rposition(&src[..j], SPACE).ok_or(Error::BadMsg)?;
             btoi::btoi(&src[i + 1..j]).map_err(|_err| Error::BadMsg)
         } else {
             btoi::btoi(&src[j + 1..k]).map_err(|_err| Error::BadMsg)
@@ -354,8 +495,8 @@ impl MCReq {
     }
 
     fn get_field(data: &[u8], offset: usize) -> Result<Range> {
-        let i = position(data, space).ok_or(Error::BadKey)?;
-        let j = position(&data[i..], space).ok_or(Error::BadKey)?;
+        let i = position(data, SPACE).ok_or(Error::BadKey)?;
+        let j = position(&data[i..], SPACE).ok_or(Error::BadKey)?;
         Ok(Range(i + offset, j + offset))
     }
 }
