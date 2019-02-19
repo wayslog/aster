665b9d534570e3b5b405e1fafbd79bb5914540ab
wayslog <zxs867179@gmail.com>
2019-02-19 11:33:21 +0800

    fixed bugs for trim_hash_tag

--- src/proxy.rs
+++ src/proxy.rs
@@ -442,18 +442,18 @@ fn fnv1a64(data: &[u8]) -> u64 {
 #[cfg(test)]
 #[test]
 fn test_trim_hash_tag() {
-    assert_eq!(trim_hash_tag(b"{}", b"abc{a}b"), b"a");
-    assert_eq!(trim_hash_tag(b"{}", b"abc{ab"), b"abc{ab");
-    assert_eq!(trim_hash_tag(b"{}", b"abc{ab}"), b"ab");
-    assert_eq!(trim_hash_tag(b"{}", b"abc{ab}asd{abc}d"), b"ab");
+    assert_eq!(trim_hash_tag(b"abc{a}b", b"{}"), b"a");
+    assert_eq!(trim_hash_tag(b"abc{ab", b"{}"), b"abc{ab");
+    assert_eq!(trim_hash_tag(b"abc{ab}", b"{}"), b"ab");
+    assert_eq!(trim_hash_tag(b"abc{ab}asd{abc}d", b"{}"), b"ab");
     assert_eq!(
-        trim_hash_tag(b"{", b"abc{ab}asd{abc}d"),
+        trim_hash_tag(b"abc{ab}asd{abc}d", b"{"),
         b"abc{ab}asd{abc}d"
     );
-    assert_eq!(trim_hash_tag(b"", b"abc{ab}asd{abc}d"), b"abc{ab}asd{abc}d");
+    assert_eq!(trim_hash_tag(b"abc{ab}asd{abc}d", b""), b"abc{ab}asd{abc}d");
     assert_eq!(
-        trim_hash_tag(b"abc", b"abc{ab}asd{abc}d"),
+        trim_hash_tag(b"abc{ab}asd{abc}d", b"abc"),
         b"abc{ab}asd{abc}d"
     );
-    assert_eq!(trim_hash_tag(b"ab", b"abc{ab}asd{abc}d"), b"");
+    assert_eq!(trim_hash_tag(b"abc{ab}asd{abc}d", b"ab"), b"");
 }
