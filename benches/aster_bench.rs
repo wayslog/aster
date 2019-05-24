162e49b87486de8fc3eef7283133f03b64d8f779
wayslog <wayslog>
2019-05-24 19:47:03 +0800

    half work of clusters

--- benches/aster_bench.rs
+++ benches/aster_bench.rs
@@ -1,64 +1,30 @@
 #[macro_use]
 extern crate criterion;
 
-use libaster::proxy::fnv::Fnv1a64;
-use libaster::proxy::ketama::HashRing;
-use libaster::redis::resp::Resp;
+// use libaster::proxy::fnv::Fnv1a64;
+// use libaster::proxy::ketama::HashRing;
+use libaster::protocol::redis::resp::MessageMut;
 
+use bytes::BytesMut;
 use criterion::Criterion;
 
 fn bench_resp(c: &mut Criterion) {
     // fn bench_parse_plain(b:&mut Bencher) {
     c.bench_function("resp parse plain", |b| {
         let sdata = "+baka for you\r\n".as_bytes();
-        b.iter(|| Resp::parse(sdata).unwrap())
+        b.iter(|| MessageMut::parse(&mut BytesMut::from(&sdata[..])).unwrap().unwrap())
     });
 
     c.bench_function("resp parse bulk", |b| {
         let sdata = "$5\r\nojbK\n\r\n".as_bytes();
-        b.iter(|| Resp::parse(sdata).unwrap())
+        b.iter(|| MessageMut::parse(&mut BytesMut::from(&sdata[..])).unwrap().unwrap())
     });
 
     c.bench_function("resp parse array", |b| {
         let sdata = "*2\r\n$1\r\na\r\n$5\r\nojbK\n\r\n".as_bytes();
-        b.iter(|| Resp::parse(sdata).unwrap())
+        b.iter(|| MessageMut::parse(&mut BytesMut::from(&sdata[..])).unwrap().unwrap())
     });
 }
 
-fn bench_ketama(c: &mut Criterion) {
-    c.bench_function("ketama get", |b| {
-        let ring = HashRing::<Fnv1a64>::new(
-            vec![
-                "redis-1".to_owned(),
-                "redis-2".to_owned(),
-                "redis-3".to_owned(),
-            ],
-            vec![10, 10, 10],
-        )
-        .expect("create new hash ring success");
-        let keys = vec![b"a".to_vec(), b"b".to_vec(), b"val-a".to_vec()];
-
-        b.iter(|| {
-            for key in &*keys {
-                let _ = ring.get_node(key);
-            }
-        })
-    });
-
-    c.bench_function("ketama add", |b| {
-        let mut ring = HashRing::<Fnv1a64>::new(
-            vec![
-                "redis-1".to_owned(),
-                "redis-2".to_owned(),
-                "redis-3".to_owned(),
-            ],
-            vec![10, 10, 10],
-        )
-        .expect("create new hash ring success");
-
-        b.iter(|| ring.add_node("redis-4".to_owned(), 10))
-    });
-}
-
-criterion_group!(benches, bench_ketama, bench_resp);
+criterion_group!(benches, bench_resp);
 criterion_main!(benches);
--- src/com.rs
+++ src/com.rs
@@ -11,3 +11,54 @@ pub enum RespError {
     #[fail(display = "message reply is bad")]
     BadReply,
 }
+
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    clusters: Vec<ClusterConfig>,
+}
+
+#[derive(Deserialize, Debug, Clone, Copy)]
+pub enum CacheType {
+    #[serde(rename = "redis")]
+    Redis,
+    #[serde(rename = "memcache")]
+    Memcache,
+    #[serde(rename = "memcache_binary")]
+    MemcacheBinary,
+    #[serde(rename = "redis_cluster")]
+    RedisCluster,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct ClusterConfig {
+    pub name: String,
+    pub listen_addr: String,
+    pub hash_tag: Option<String>,
+
+    pub thread: Option<usize>,
+    pub cache_type: CacheType,
+
+    pub read_timeout: Option<u64>,
+    pub write_timeout: Option<u64>,
+
+    pub servers: Vec<String>,
+
+    // cluster special
+    pub fetch: Option<u64>,
+    pub read_from_slave: Option<bool>,
+
+    // proxy special
+    pub ping_fail_limit: Option<usize>,
+    pub ping_interval: Option<usize>,
+
+    // dead codes
+
+    // command not support now
+    pub dial_timeout: Option<u64>,
+    // dead option: not support other proto
+    pub listen_proto: Option<String>,
+
+    // dead option: always 1
+    pub node_connections: Option<usize>,
+}
