3baf2073fc239b609fa951d3483e5e28b6e658a9
wayslog <zxs867179@gmail.com>
2019-11-04 15:37:06 +0800

    add measure redis for adam

--- a2/adam/src/main.rs
+++ a2/adam/src/main.rs
@@ -1,3 +1,35 @@
+use failure::Error;
+
+use std::time::Instant;
+use redis::Client;
+
+use redis::Commands;
+
+use std::env;
+
+
+fn measure_redis(addr: &str, max: usize) -> Result<(), Error> {
+    let cli = Client::open(&*format!("redis://{}", addr))?;
+    let mut conn = cli.get_connection()?;
+    let start = Instant::now();
+    for i in 0..max {
+        let _: () = conn.set(i, i)?;
+    }
+    let mut sum = 0;
+    for i in 0..max {
+        let ival: i32 = conn.get(i)?;
+        sum += ival;
+    }
+    let dur = start.elapsed();
+    println!("measure redis {} with dur {:?} and sum is {}", addr, dur, sum);
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    args.next().unwrap();
+    let addr = args.next().expect("addr must exists");
+    let count = args.next().unwrap_or("10".to_string());
+    let count = count.parse::<usize>().unwrap_or(10);
+    measure_redis(&addr, count).unwrap();
 }
