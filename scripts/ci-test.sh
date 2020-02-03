4e2587c340955f7eb99d870d6db8029da5ca2417
wayslog <zxs867179@gmail.com>
2020-02-03 16:06:07 +0800

    ci happy and debug

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "aster start tests......"
 
-cargo test
+# cargo test
 
 docker run -e "IP=0.0.0.0" -d -p 7000-7007:7000-7007 grokzen/redis-cluster:5.0.7 && cargo test --verbose --all
 make debug &
@@ -13,8 +13,8 @@ sudo apt install python3 -y
 git clone --depth=1 https://github.com/wayslog/redis-py.git
 cp default.toml redis-py
 cd redis-py
-pip3 install pytest mock
-python3 setup.py install
+pip install pytest mock
+python setup.py install
 
 pytest --redis-url="redis://127.0.0.1:7787"
 pytest --redis-url="redis://127.0.0.1:7788"
