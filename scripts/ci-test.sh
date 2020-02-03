e0920840e67e42b2ef20f50167ec26084c93887a
wayslog <zxs867179@gmail.com>
2020-02-03 16:22:01 +0800

    add python toml dependencis

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -8,7 +8,7 @@ echo "aster start tests......"
 docker run -e "IP=0.0.0.0" -d -p 7000-7007:7000-7007 grokzen/redis-cluster:5.0.7 && cargo test --verbose --all
 
 sudo apt install python3 -y
-sudo pip install pytest mock
+sudo pip install pytest mock python-toml
 
 git clone --depth=1 https://github.com/wayslog/redis-py.git
 cp default.toml redis-py
