a801ae2a19cb5e4f4edf7fb664d4f2baf9b885ea
wayslog <zxs867179@gmail.com>
2020-02-03 15:11:24 +0800

    add travis test of ci

---
+++ scripts/ci-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+docker run -e "IP=0.0.0.0" -d -p 7000-7007:7000-7007 grokzen/redis-cluster:5.0.7 && cargo test --verbose --all
+make debug &
+
+git clone --depth=1 https://github.com/wayslog/redis-py.git
+cp default.toml redis-py
+cd redis-py
+pip3 install pytest mock
+python3 setup.py install
+
+pytest --redis-url="redis://127.0.0.1:7787"
+pytest --redis-url="redis://127.0.0.1:7788"
